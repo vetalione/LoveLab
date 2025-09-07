1514ffc9a381ca6ffa42a5dfddd051b9fca28589
Виталий Смирнов <legend@Vitalijs-MacBook-Pro.local>
2025-09-07 18:49:58 +0800

    refactor: rename main component to RelationshipLab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RelationshipLab from './lovelab_fixed_connect.jsx';
+import RelationshipLab from './RelationshipLab.jsx';
 
 export default function App() {
   return <RelationshipLab />;
